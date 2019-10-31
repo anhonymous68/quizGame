c1d9823872b44c343d649cde8cecde02250a3747
Tuan Le <letuanlt09@gmail.com>
2019-10-31 17:10:12 -0400

    displayData

---
+++ PuzzelGame/Assets/Data.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Data
+{ 
+    public class Data : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+        private Dictionary<string, string> data;
+
+        Data()
+        {
+            data = new Dictionary<string, string>();
+            data.Add("Student", "A person who is studying at a school or college");
+            data.Add("Teacher", "A person who teaches, especially in a school");
+            data.Add("Lawyer", "A person who practices or studies law; an attorney or a counselor");
+            data.Add("Doctor", "A qualified practitioner of medicine; a physician");
+        }
+
+        public Dictionary<string, string> getData()
+        {
+            return data;
+        }
+    }
+}
---
+++ PuzzelGame/Assets/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Dictionary<string, string> data;
+    public GameObject[] key = new GameObject[4];
+    public GameObject[] value = new GameObject[4];
+
+    void Start()
+    {
+        data = new Dictionary<string, string>();
+        data.Add("Student", "A person who is studying at a school or college");
+        data.Add("Teacher", "A person who teaches, especially in a school");
+        data.Add("Lawyer", "A person who practices or studies law; an attorney or a counselor");
+        data.Add("Doctor", "A qualified practitioner of medicine; a physician");
+        int index = 0;
+        foreach (KeyValuePair<string, string> i in data)
+        {
+            key[index].GetComponent<Text>().text = i.Key;
+            value[index].GetComponent<Text>().text = i.Value;
+            index++;
+
+
+        }
+
+    }
+    
+    
+
+}
